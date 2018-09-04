eee827dba748de1eac578d0ffc9ad0593a557436
VonSieger <claasvonderschen@riseup.net>
2018-09-04 22:24:09 +0000

    Using Queue, to store tracks, which should be downloaded

--- src/converter/SYConverter.java
+++ src/converter/SYConverter.java
@@ -1,5 +1,10 @@
 package converter;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 import dataTypes.Track;
 import spotify.TitleFinder;
@@ -13,16 +18,31 @@ import youtube.YTSearch;
 public class SYConverter {
 
 	public static void main(String[] args) throws IOException {
-		Track currentTrack = TitleFinder.getCurrentTrack();
-		if(currentTrack.getArtist() == null || currentTrack.getTitle() == null) {
-			System.out.println("No Track was found");
-			System.exit(1);
+		
+		Queue<Track> tracks = new LinkedList<Track>();
+		
+		if(args.length == 0 || args[0].contains("current")) {
+			Track currentTrack = TitleFinder.getCurrentTrack();
+			if(currentTrack.getArtist() == null || currentTrack.getTitle() == null) {
+				System.out.println("No Track was found");
+				System.exit(1);
+			}
+			currentTrack.setUrl(YTSearch.searchYouTube(currentTrack.getTitle() + " " + currentTrack.getArtist()));
+			tracks.add(currentTrack);
+		}else if(args[0].contains("playlist") && args.length == 2) {
+			
 		}
-		currentTrack.setUrl(YTSearch.searchYouTube(currentTrack.getTitle() + " " + currentTrack.getArtist()));
-		YTDownloader downloader = new YTDownloader(currentTrack);
-		Thread download = new Thread(downloader);
-		download.start();
-		System.out.println(currentTrack.getUrl() + " is getting downloaded...");
+		
+		Queue<Thread> downloads = new ArrayDeque<Thread>(tracks.size());
+		
+		while(!tracks.isEmpty()) {
+			YTDownloader downloader = new YTDownloader(tracks.peek());
+			Thread download = new Thread(downloader);
+			download.start();
+			System.out.println(tracks.poll().getUrl() + " is getting downloaded...");
+			downloads.add(download);
+		}
+		
 		//TODO print exit status
 	}
 
