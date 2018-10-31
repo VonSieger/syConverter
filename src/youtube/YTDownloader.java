18e03a5e625b4b6ee9cea5eeead1a9b204ef25d5
VonSieger <claasvonderschen@riseup.net>
2018-10-31 15:09:07 +0000

    Add tagging for downloaded tracks. Artist and Title fields are supported.

--- src/youtube/YTDownloader.java
+++ src/youtube/YTDownloader.java
@@ -4,6 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.jaudiotagger.audio.exceptions.CannotWriteException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.audio.mp3.MP3File;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.TagException;
+import org.jaudiotagger.tag.id3.ID3v24Tag;
 
 import dataTypes.Track;
 
@@ -39,9 +48,31 @@ public class YTDownloader implements Runnable{
 					"-o" + musicTrack.getTitle() + "-" + musicTrack.getArtists()[0] + ".%(ext)s'",
 					"-x", "--audio-format", "mp3",
 					musicTrack.getUrl()).start();
+			youtubeDL.waitFor();
+			
+			//create ID3 v2.4 Tag for downloaded mp3 file
+			MP3File downloadMP3 = new MP3File(System.getProperty("user.dir") + "/" + musicTrack.getTitle() + 
+					"-" + musicTrack.getArtists()[0] + ".mp3");
+			ID3v24Tag v24Tag = (ID3v24Tag)downloadMP3.getID3v2TagAsv24();
+			
+			//create ";"-seperated list of artist and store it into ID3 Tag
+			String artists = "";
+			for(String artist : musicTrack.getArtists())
+				artists += artist + ";";
+			artists = artists.substring(0, artists.length() -1);
+			v24Tag.addField(FieldKey.ARTIST, artists);
+			
+			v24Tag.addField(FieldKey.TITLE, musicTrack.getTitle());
+			
+			downloadMP3.commit();
+			
 			return youtubeDL;
 		} catch (IOException e) {
 			e.printStackTrace();
+		}catch(TagException| ReadOnlyFileException | InvalidAudioFrameException | CannotWriteException e) {
+			//TODO improve exception handling
+			e.printStackTrace();
+		} catch (InterruptedException e) {
 		}
 		return null;
 	}
